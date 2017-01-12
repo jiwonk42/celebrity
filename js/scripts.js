88442d9b56e7ef929e6cf0e07f246a71ed03f39a
jiwon kang and matt caswell <jiwon@>
2017-01-12 15:11:32 -0800

    finalized celebrity matching date website

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,24 @@ $(document).ready(function() {
 
     $(".celebrity").hide();
     var age = parseInt($("input#age").val());
+    var gender = $("select#gender").val();
 
     if(age) {
-      if(age > 60) {
+      if(age > 60 && gender === "female") {
         $("#jennifer").show();
-      }
-      else if (age <= 60 && age > 40) {
+      } else if(age > 60 && gender === "male") {
+        $("#arnold").show();
+      } else if (age <= 60 && age > 40 && gender === "female") {
+        $("#sharon").show();
+      } else if(age <= 60 && age > 40 && gender === "male") {
         $("#rock").show();
-      }
-      else if (age <= 40 && age > 20) {
+      } else if (age <= 40 && age > 20 && gender === "female") {
         $("#gabrielle").show();
-      }
-      else {
+      } else if (age <= 40 && age > 20 && gender === "male") {
+        $("#mark").show();
+      } else if (age <= 20 && gender === "female") {
+        $("#selena").show();
+      } else if (age <= 20 && gender === "male") {
         $("#justin").show();
       };
     } else {
